ca10eba8474074811c3c31985dcafc75a2730171
Tucker Brookshire <tuckerbrookshire@gmail.com>
2021-02-03 03:57:30 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,34 @@
+def using_push(array, string)
+  array.push(string)
+end
+
+def using_unshift(array, string)
+ bouroughs_in_nyc = ["Brooklyn", "Queens", "Manhattan", "Bronx"]
+ bouroughs_in_nyc.unshift("Staten Island")
+end
+
+def using_pop(array)
+  array.pop
+end
+
+def pop_with_args(array)
+  array.pop(2)
+end
+
+def using_shift(array)
+  array.shift
+end
+
+def shift_with_args(array)
+  ice_cream_brands = ["Blue Bell Creameries", "Ben & Jerry's", "Baskin Robbins", "Braum's", "Breyer's"]
+  ice_cream_brands.shift(2)
+end
+
+
+
+
+
+
+
+
+
